1301d111f971a4cd74ceb0539d6c0951e29780f1
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-08 20:32:17 +0300

    tap-shy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.*;
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person1 = new Person("Akyl", 20);
+        Person person2 = new Person("Erlan", 30);
+        Person person3 = new Person("Baatyr", 29);
+        Person person4 = new Person("Manas", 31);
+
+
+        HashSet<Person> hashSet = new HashSet<>();
+        hashSet.add(person1);
+        hashSet.add(person2);
+
+
+        TreeSet<Person> treeSet = new TreeSet<>(hashSet);
+        hashSet.clear();
+        System.out.println(hashSet);
+        treeSet.add(person3);
+        treeSet.add(person4);
+            int a = 0;
+            for (Person p: treeSet) {
+            int b =p.getAge() ;
+            a += b;
+            System.out.println(p);
+            System.out.println(a);
+
+
+        }
     }
 }
+
+
+
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Person {
+import java.util.Comparator;
+
+public class Person implements Comparable<Person>{
+    public static Object sortCountryId;
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                " name = " + name  +','+
+                "  age = " + age +
+               '}';
+    }
+
+
+    @Override
+    public int compareTo(Person o) {
+        return this.age-o.age;
+    }
 }
